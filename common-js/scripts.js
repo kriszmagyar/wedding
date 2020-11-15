d09bc14c2e9cb9a4dc21cf12eddee04c940e5747
Krisz Magyar <krisz.magyar@gmail.com>
2020-11-15 18:04:27 +0100

    add smooth scrolling

--- common-js/scripts.js
+++ common-js/scripts.js
@@ -6,6 +6,11 @@
 		return;
 	});
 
+	$('a').on('click', function(event) {
+		var target = event.target.hash;
+		doScrolling(target, 300);
+	})
+
 	countdownTime();
 	
 	$('[data-nav-menu]').on('click', toggleVisible);
@@ -14,6 +19,34 @@
 	});
 	
 })(jQuery);
+
+function doScrolling(element, duration) { 
+	var startingY = window.pageYOffset;
+	var elementY = getElementY(element);
+	var diff = elementY - startingY;
+	var start;
+  
+	// Bootstrap our animation - it will get called right before next frame shall be rendered.
+	window.requestAnimationFrame(function step(timestamp) {
+	  if (!start) start = timestamp;
+	  // Elapsed milliseconds since start of scrolling.
+	  var time = timestamp - start;
+	  // Get percent of completion in range [0, 1].
+	  var percent = Math.min(time / duration, 1);
+  
+	  window.scrollTo(0, startingY + diff * percent);
+  
+	  // Proceed with animation as long as we wanted it to.
+	  if (time < duration) {
+		window.requestAnimationFrame(step);
+	  }
+	})
+}
+
+function getElementY(query) {
+	if (query === '#' || query === '') return 0;
+	return window.pageYOffset + document.querySelector(query).getBoundingClientRect().top
+  }
 
 function toggleVisible() {
 	$($(this).data('nav-menu')).toggleClass('visible');
