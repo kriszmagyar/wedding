d84fb0a76a36ae5da18aa97097547b77e587801e
Krisz Magyar <krisz.magyar@gmail.com>
2020-11-15 17:45:36 +0100

    hide mobile menu after click

--- common-js/scripts.js
+++ common-js/scripts.js
@@ -1,36 +1,25 @@
-
-	
 (function ($) {
 
     "use strict";
 
-	$('a[href="#"]').on('click', function(event){
+	$('a[href="#"]').on('click', function() {
 		return;
 	});
 
 	countdownTime();
 	
-	$('[data-nav-menu]').on('click', function(event){
-		var $this = $(this),
-			visibleHeadArea = $this.data('nav-menu');
-		$(visibleHeadArea).toggleClass('visible');
-	});
-	
-	
-	var winWidth = $(window).width();
-	dropdownMenu(winWidth);
-	
-	$(window).on('resize', function(){
-		dropdownMenu(winWidth);
-		
+	$('[data-nav-menu]').on('click', toggleVisible);
+	$('#main-menu>li>a').on('click', function() {
+		$('#main-menu').removeClass('visible');
 	});
 	
 })(jQuery);
 
-
+function toggleVisible() {
+	$($(this).data('nav-menu')).toggleClass('visible');
+}
 
 function countdownTime(){
-	
 	if(isExists('#clock')){
 		$('#clock').countdown('2021/04/17', function(event){
 			var $this = $(this).html(event.strftime(''
@@ -41,39 +30,7 @@ function countdownTime(){
 		});
 	}
 }
-function dropdownMenu(winWidth){
-	
-	if(winWidth > 767){
-		
-		$('.main-menu li.drop-down').on('mouseover', function(){
-			var $this = $(this),
-				menuAnchor = $this.children('a');
-				
-			menuAnchor.addClass('mouseover');
-			
-		}).on('mouseleave', function(){
-			var $this = $(this),
-				menuAnchor = $this.children('a');
-				
-			menuAnchor.removeClass('mouseover');
-		});
-		
-	} else {
-		
-		$('.main-menu li.drop-down > a').on('click', function(){
-			
-			if ($(this).attr('href') == '#') return false;
-			if ($(this).hasClass('mouseover')){ $(this).removeClass('mouseover'); }
-			else { $(this).addClass('mouseover'); }
-			return false;
-		});
-	}
-	
-}
 
-function isExists(elem){
-	if ($(elem).length > 0) { 
-		return true;
-	}
-	return false;
+function isExists(elem) {
+	return $(elem).length > 0;
 }
